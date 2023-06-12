2c308271408677e33829c9c62c1e0c0d1ab6f8c8
PRINCE <pdoshi135@gmail.com>
2023-06-12 18:17:37 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */
+module.exports = {
+  experimental: {
+    appDir: true,
+  },
+  webpack(config, { webpack }) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+  images: {
+    domains: ['cdn.imagin.studio'],
+  },
+};
